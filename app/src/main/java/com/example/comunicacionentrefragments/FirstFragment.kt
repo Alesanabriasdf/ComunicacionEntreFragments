407fd64c7af3a22af4ef625f92c8ac1937ddc1ed
Alejandro Sanabria <alejandro.sanabria@bancogalicia.com.ar>
2022-12-15 08:37:05 -0300

    APP for comunication fragments

---
+++ app/src/main/java/com/example/comunicacionentrefragments/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.example.comunicacionentrefragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class FirstFragment : Fragment() {
+
+    private var listener: FirstFragmentListener? = null
+    private lateinit var btnShowContent: Button
+
+    fun setListener(listener: FirstFragmentListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        initializeButton(view)
+
+        return view
+    }
+
+    private fun initializeButton(view: View) {
+        btnShowContent = view.findViewById(R.id.btnShowContent)
+        btnShowContent.setOnClickListener {
+            listener?.onShowContentClick()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/comunicacionentrefragments/FirstFragmentListener.kt
@@ -0,0 +1,5 @@
+package com.example.comunicacionentrefragments
+
+interface FirstFragmentListener {
+    fun onShowContentClick()
+}
